adbd516cabe0c9f41d95269d684123f0e9ee3d8c
Emmanuil <e.kovshilov@gmail.com>
2020-08-13 20:19:43 +0300

    Убрал модификаторы, переписал св-ва в элементы блоков, выделил элементы в отдельные блоки, переименовал элемент в модификатор, поменял порядок св-в css

--- source/js/menu.js
+++ source/js/menu.js
@@ -3,6 +3,6 @@
   var menuToggle = document.querySelector('.js-menu-toggle');
 
   menuToggle.addEventListener('click', function (event) {
-    menu.classList.toggle('main-nav__active');
+    menu.classList.toggle('main-nav--active');
   });
 })();
